631d7a0dca2416e7bd7b936b537964aa958f8e2d
cgdev2019 <cgdev.sasu@gmail.com>
2025-10-31 09:35:32 +0100

    Note remote push limitation

--- proxy.ts
+++ proxy.ts
@@ -1,10 +1,16 @@
+import { NextRequest } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
+
 import { defaultLocale, locales } from '@/lib/i18n/config';
 
-export default createMiddleware({
+const intlProxy = createMiddleware({
   defaultLocale,
   locales,
 });
+
+export default function proxy(request: NextRequest) {
+  return intlProxy(request);
+}
 
 export const config = {
   matcher: [String.raw`/((?!api|_next|.*\..*).*)`],
