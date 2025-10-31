908e1d49821580410b8e99527cf921f00c3b78a9
cgdev2019 <cgdev.sasu@gmail.com>
2025-10-31 18:41:18 +0100

    Await tax route params before rendering

--- app/[locale]/(app)/taxes/[code]/page.tsx
+++ app/[locale]/(app)/taxes/[code]/page.tsx
@@ -1,9 +1,11 @@
 import { TaxDetail } from '@/features/taxes/components/TaxDetail';
 
 type Props = {
-  params: { code: string };
+  params: Promise<{ code: string }>;
 };
 
-export default function TaxDetailPage({ params }: Props) {
-  return <TaxDetail code={params.code} />;
+export default async function TaxDetailPage(props: Props) {
+  const { code } = await props.params;
+
+  return <TaxDetail code={code} />;
 }
